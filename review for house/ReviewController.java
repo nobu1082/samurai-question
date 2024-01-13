0b0c27580029a2fbb71f5f42a58d33138b8ac67b
nobu1082 <nk.kusaba@gmail.com>
2024-01-14 08:54:01 +0900

    Add files via upload

--- review for house/ReviewController.java
+++ review for house/ReviewController.java
@@ -2,6 +2,7 @@ package com.example.samuraitravel.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -86,14 +87,18 @@ public class ReviewController {
   		return "reviews/detail";
   	}
   	
+  	/**
   	@GetMapping("/reviews/{houseid}")
   	public String show(@PathVariable(name = "houseid")Integer houseid, Model model) {
   		Review reviewhouse = reviewRepository.getReferenceById(houseid);
   		model.addAttribute("reviewhouse",reviewhouse);
   		
   		return "reviews/show";
+  	} **/
+  	@GetMapping("reviews/{houseid}")
+  	public Optional<Review> getReviewByhouseId(@PathVariable Integer houseid){
+  		return reviewService.getReviewByHouseId(houseid);
   	}
-  	
   	
  }
 
