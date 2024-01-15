6f73d93cace9dbfcb65a9e0176efb25cca0e96af
nobu1082 <nk.kusaba@gmail.com>
2024-01-15 23:46:50 +0900

    Add files via upload

---
+++ review for houses4/repository/ReviewRepository.java
@@ -0,0 +1,19 @@
+package com.example.samuraitravel.repository;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.samuraitravel.entity.Review;
+
+@Repository
+ public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	
+	List<Review> findByHouseid(Integer houseid);
+	
+	
+	
+}
